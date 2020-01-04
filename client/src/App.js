0a33000ec7d92331b413d665dfb7c5db41beb0bc
alex629lee <alex629lee@yahoo.com>
2020-01-03 18:44:50 -0800

    Fix resizing issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Game from "./components/Game";
 import MIDISounds from 'midi-sounds-react';
+import $ from "jquery";
 
 const App = () =>  {
   return (
@@ -12,6 +13,44 @@ const App = () =>  {
     </div>
   );
 }
+
+function onElementHeightChange(elm, callback) {
+  var lastHeight = elm.clientHeight, newHeight;
+  (function run() {
+    newHeight = elm.clientHeight;
+    if (lastHeight != newHeight)
+      callback(newHeight)
+    lastHeight = newHeight
+
+    if (elm.onElementHeightChangeTimer)
+      clearTimeout(elm.onElementHeightChangeTimer)
+
+    elm.onElementHeightChangeTimer = setTimeout(run, 200)
+  })()
+}
+
+
+
+
+onElementHeightChange(document.body, function (h) {
+  console.log('Body height changed:', h);
+  let cols = Array.from(document.getElementsByClassName("column-container"));
+  console.log(cols);
+  document.body.height = h + "px";
+  if (cols.length > 0) {
+    cols.forEach(col => {
+      col.height = h + "px";
+    })
+  }
+  let stageSelectMain = Array.from(document.getElementsByClassName("stage-select-main"));
+  if (stageSelectMain.length > 0) {
+    stageSelectMain[0].height = h + "px";
+  }
+  let selectPageContainer = Array.from(document.getElementsByClassName("stage-select-page-container"));
+  if (selectPageContainer.length > 0) {
+    selectPageContainer[0].height = h + "px";
+  }
+});
 
 
 export default App;
