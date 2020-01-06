c6f19a885c5a10dd0d45e96dcc9a511348abe485
alex629lee <alex629lee@yahoo.com>
2020-01-06 12:24:50 -0800

    Add about me modal with github and linkedin links

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -60,14 +60,71 @@ class Game extends React.Component {
     this.restart();
   }
 
+  handleInfoModule(action) {
+    switch(action) {
+      case "INFO BUTTON":
+        if (!this.state.infoShowing) {
+          this.setState({ infoShowing: true });
+          $(".info-modal").css("display", "flex");
+          $(".info-modal").css("z-index", 0);
+          setTimeout(() => $(".info-modal").animate({ opacity: 1 }, 300));
+          break;
+        } else {
+          this.setState({ infoShowing: false });
+          $(".info-modal").animate({ opacity: 0 }, 300);
+          setTimeout(() => $(".info-modal").css("display", "none"), 300);
+          return;
+        }
+      case "EXIT BUTTON":
+        this.setState({ infoShowing: false });
+        $(".info-modal").animate({ opacity: 0 }, 300);
+        setTimeout(() => $(".info-modal").css("display", "none"), 300);
+        break;
+      case "BOX":
+        break;
+    }
+  }
+
   render() {
     if (!this.state.stageNum) {
       return (
         <div className="stage-select-main">
           <div className="stage-select-page-container">
-            
+            <div className="info-modal">
+              <div className="info-module-container" onClick={() => this.handleInfoModule("BOX")}>
+                {/* <div className="info-title">Created by</div> */}
+                <div className="info-pic-name">
+                  <img className="info-module-pic" 
+                    src={process.env.PUBLIC_URL + '/images/me.jpg'}>
+                  </img>
+                  <div className="info-name-links">
+                    <div>Alex Lee</div>
+                    <a href="https://github.com/alex629lee"><i class="fab fa-github"></i> Github</a>
+                    <a href="https://www.linkedin.com/in/alex-lee-b09a7310a/"><i class="fab fa-linkedin"></i> LinkedIn</a>
+                  </div>
+                </div>
+                <img className="info-module-given-pic"
+                  src={process.env.PUBLIC_URL + '/images/given-transparent.png'}>
+                </img>
+              </div>
+            </div>
+
+            {this.state.infoShowing ? (
+              <i className="fas fa-times-circle" 
+                onClick={() => this.handleInfoModule("EXIT BUTTON")}>
+              </i>
+            ) : (
+              <i className="fas fa-info-circle" onClick={() => {
+                this.handleInfoModule("INFO BUTTON")
+              }}></i>
+            )}
+
+
             <div className="stage-select-container">
-              <div className="stage-select-page-title"><img src={process.env.PUBLIC_URL + '/images/guitar1.png'} /><div>YUKI'S GUITAR</div></div>
+              <div className="stage-select-page-title">
+                <img src={process.env.PUBLIC_URL + '/images/guitar1.png'} />
+              <div>YUKI'S GUITAR</div></div>
+
               <div className="stage-select">
                 <h2>SELECT STAGE</h2>
                 <ul className="stage-select-list">
