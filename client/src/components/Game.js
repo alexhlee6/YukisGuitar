678da711f7d6508b25e3498d523131aa3a0e9962
alex629lee <alex629lee@yahoo.com>
2020-01-25 19:54:35 -0800

    Add how to play module

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -9,7 +9,8 @@ class Game extends React.Component {
     this.state = {
       started: false, 
       stageNum: null,
-      loading: true
+      loading: true,
+      instructonsShowing: false
     };
   }
 
@@ -66,7 +67,9 @@ class Game extends React.Component {
         if (!this.state.infoShowing) {
           this.setState({ infoShowing: true });
           $(".info-modal").css("display", "flex");
-          $(".info-modal").css("z-index", 0);
+          // $(".info-modal").css("z-index", 0);
+          // $(".stage-select-container").animate({ opacity: 0 }, 300);
+          $(".fa-times-circle").css("z-index", 3);
           setTimeout(() => $(".info-modal").animate({ opacity: 1 }, 300));
           break;
         } else {
@@ -79,6 +82,7 @@ class Game extends React.Component {
         this.setState({ infoShowing: false });
         $(".info-modal").animate({ opacity: 0 }, 300);
         setTimeout(() => $(".info-modal").css("display", "none"), 300);
+        // $(".stage-select-container").animate({opacity: 1}, 300);
         break;
       case "BOX":
         break;
@@ -86,13 +90,48 @@ class Game extends React.Component {
   }
 
   render() {
+
+    let stageSelectContent = this.state.instructonsShowing ? (
+      <div className="stage-select-container">
+        <div className="how-to-play">
+          <div className="how-to-button" onClick={() => this.setState({ instructonsShowing: false })}>
+            Back to Select <i className="fas fa-chevron-right"></i>
+          </div>
+          <h3>How to Play</h3>
+          <ul>
+            <li>Choose one of the three songs on the select screen to start.</li>
+            <li>When a music note reaches the bottom of a column, activate the matching button.</li>
+            <li>Aim for a higher score by accurately timing your plays!</li>
+          </ul>
+        </div>
+      </div>
+    ) : (
+      <div className="stage-select-container">
+        <div className="how-to-button" onClick={() => this.setState({ instructonsShowing: true })}>
+          How to Play <i className="fas fa-chevron-right"></i>
+        </div>
+        <div className="stage-select-page-title">
+          <img src={process.env.PUBLIC_URL + '/images/guitar1.png'} />
+          <h1>YUKI'S GUITAR</h1>
+        </div>
+
+        <div className="stage-select">
+          <h2>SELECT STAGE</h2>
+          <ul className="stage-select-list">
+            <li key="song-1" onClick={() => this.setStage(1)}><i className="fas fa-play"></i>Marutsuke - <span> Easy</span></li>
+            <li key="song-2" onClick={() => this.setStage(2)}><i className="fas fa-play"></i>Fuyu no Hanashi - <span> Medium</span></li>
+            <li key="song-3" onClick={() => this.setStage(3)}><i className="fas fa-play"></i>Kizuato - <span> Hard</span></li>
+          </ul>
+        </div>
+      </div>
+    );
+
     if (!this.state.stageNum) {
       return (
         <div className="stage-select-main">
           <div className="stage-select-page-container">
             <div className="info-modal">
               <div className="info-module-container" onClick={() => this.handleInfoModule("BOX")}>
-                {/* <div className="info-title">Created by</div> */}
                 <div className="info-pic-name">
                   <img className="info-module-pic" 
                     src={process.env.PUBLIC_URL + '/images/me.jpg'}>
@@ -120,21 +159,7 @@ class Game extends React.Component {
               }}></i>
             )}
 
-
-            <div className="stage-select-container">
-              <div className="stage-select-page-title">
-                <img src={process.env.PUBLIC_URL + '/images/guitar1.png'} />
-              <div>YUKI'S GUITAR</div></div>
-
-              <div className="stage-select">
-                <h2>SELECT STAGE</h2>
-                <ul className="stage-select-list">
-                  <li key="song-1" onClick={() => this.setStage(1)}><i className="fas fa-play"></i>Marutsuke</li>
-                  <li key="song-2" onClick={() => this.setStage(2)}><i className="fas fa-play"></i>Fuyu no Hanashi</li>
-                  <li key="song-3" onClick={() => this.setStage(3)}><i className="fas fa-play"></i>Kizuato</li>
-                </ul>
-              </div>
-            </div>
+            { stageSelectContent }
           </div>
         </div>
       )
